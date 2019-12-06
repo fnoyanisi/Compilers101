920c3df5fbc4deb6e57b1aaaed0fd658505133b6
Fehmi Noyan Isi <fnoyanisi@yahoo.com>
2019-12-06 13:25:31 +1300

    Adding the Kestrel Compiler project

---
+++ kestrelc/src/lexical.h
@@ -0,0 +1,42 @@
+/*-
+ * lexical.h 
+ * Copyright (c) 2019 Fehmi Noyan Isi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+
+ typedef enum {IDENT, KEYWORD, NUMBER, STRING, PUNC, ENDFILE} lex_types;
+
+ typedef struct lexeme_t {
+     lex_types type;
+     uint32_t value;
+ } lexeme;
+
+ extern lexeme lex_this;    /* the current lexeme */
+ extern lexeme lex_next;    /* the next lexeme */
+
+ void lex_open(char *f);
+ void lex_advance();
+ void lex_put(lexeme *l, FILE *f);
