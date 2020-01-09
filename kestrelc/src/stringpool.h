3db66876f5aee9f2f7c988296915f26902971121
Fehmi Noyan Isi <fnoyanisi@yahoo.com>
2020-01-10 09:03:45 +1300

    updated comments

---
+++ kestrelc/src/stringpool.h
@@ -0,0 +1,68 @@
+/*-
+ * stringpool.h
+ * Copyright (c) 2019 Fehmi Noyan Isi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifndef EXTERN
+    #define EXTERN extern
+#endif
+
+#define POOL_SIZE   2048
+#define STRING_NUL  0
+
+/* an index within [0 POOL_SIZE-1] range to address the strings */
+typedef uint32_t string_handle;
+
+/* initializer for the string pool */
+void string_init();
+
+/* 
+ * set up to accumulate a new string and return its handle 
+ * the line parameter is for error reporting
+ */
+string_handle string_start(int line);
+
+/* append a char to the current string */
+void string_append(char ch);
+
+/* mark the end of the string */
+void string_done();
+
+/* accept the new string as a permament part of the pool */
+void string_accept();
+
+/* reject the new string, it will not be added to the pool */
+void string_reject();
+
+/* outputs the text of the string to FILE */
+void string_put(string_handle h, FILE *f);
+
+/* compare two strings */
+bool string_eq(string_handle h1, string_handle h2);
+
+#undef EXTERN
